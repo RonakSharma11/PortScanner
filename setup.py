eeedc8cc8ee2c23a5d5ac9014e672dc9a3632bce
RonakSharma11 <110310259+RonakSharma11@users.noreply.github.com>
2024-11-29 23:02:46 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='OpenPort',  # Name of the package
+    version='0.1',  # Version of the package
+    packages=find_packages(),  # Automatically find packages in the directory
+    install_requires=[
+        'scapy',  # Required dependency for ARP scanning
+    ],
+    entry_points={
+        'console_scripts': [
+            'port-scanner=PortScanner:main',  # Entry point for the command-line interface
+        ],
+    },
+    description='A simple Python port scanner that discovers live hosts in a subnet and scans for open ports.',
+    author='Ronak',  # Replace with your name
+    author_email='ronak.d.sharma111@gmail.com',  # Replace with your email
+    url='https://github.com/RonakSharma11/PortScanner',  # Replace with your GitHub repository URL
+)
